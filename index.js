86321dd3b4806b5ddc473d270aa2e5799d437b5a
Tim <tbrandle53@gmail.com>
2016-12-06 10:38:26 -0700

    Add button event listener to change the text

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var textToChange = document.querySelector('.text-to-change')
 var buttonForText = document.querySelector('#text-button');
+
+buttonForText.addEventListener('click', function(){
+  textToChange.innerText = "You've Changed!";
+})
